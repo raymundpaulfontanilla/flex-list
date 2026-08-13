163e0ca3791f041071e3ef77acf4c8222780fa14
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2026-08-13 21:45:23 +0800

    add delete task destructuring custom hook method

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function Dashboard() {
-  const { errors, isLoading, tasks, createTask, updateTask } = useFetch();
+  const { errors, isLoading, tasks, createTask, updateTask, deleteTask } =
+    useFetch();
   const navigate = useNavigate();
   const [userName, setUserName] = useState(null);
   const [newTaskTitle, setNewTaskTitle] = useState("");
