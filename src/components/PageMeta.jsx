8f61b66bf4705b8e139012aed0ed9c21b6a9c604
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2026-08-14 12:06:49 +0800

    add page meta component title and favicon dynamic

---
+++ src/components/PageMeta.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+function PageMeta({ title, faviconUrl }) {
+  useEffect(() => {
+    if (title) {
+      document.title = `${title} | Flex List`;
+    }
+
+    if (faviconUrl) {
+      let link = document.querySelector("link[rel~='icon']");
+
+      if (!link) {
+        link = document.createElement("link");
+        link.rel = "icon";
+        document.head.appendChild(link);
+      }
+
+      link.href = faviconUrl;
+    }
+  }, [title, faviconUrl]);
+
+  return null;
+}
+
+export default PageMeta;
